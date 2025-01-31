9eae5c4c258eb6b1bb0f2a813fe22d7adb38fa71
damian14377 <damian4ev@gmail.com>
2025-01-31 01:38:34 +0100

    Update generate_charts.py

--- generate_charts.py
+++ generate_charts.py
@@ -24,7 +24,7 @@ def generate_charts(file_path, output_folder="output"):
     fig, ax = plt.subplots(figsize=(12, 6))  # Tworzenie figury i osi
     bars = plt.bar(df_sorted["Player Name"], df_sorted["Distance (km)"], color="#FFAF00")
     plt.ylabel("Dystans (km)")
-    fig.text(0.05, 0.2, f"vs {session_title}", ha='left', va='bottom', fontsize=18, fontweight='bold')
+    fig.text(0.05, 0.90, f"vs {session_title}", ha='left', va='bottom', fontsize=18, fontweight='bold')
     plt.title("Dystans (km)", fontsize=14, pad=10)
     for bar in bars:
         height = bar.get_height()
