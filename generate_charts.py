d9c87e8a6e193ce627f25decb4f13bc6ca8c5d46
damian14377 <damian4ev@gmail.com>
2025-01-31 01:25:23 +0100

    Update generate_charts.py

--- generate_charts.py
+++ generate_charts.py
@@ -21,7 +21,7 @@ def generate_charts(file_path, output_folder="output"):
     # Posortowanie danych
     df_sorted = df_filtered.sort_values(by="Distance (km)", ascending=False)
     total_distance = df_sorted["Distance (km)"].sum()
-    fig, ax = plt.figure(figsize=(12, 6))  # Tworzenie figury i osi
+    fig, ax = plt.subplots(figsize=(12, 6))  # Tworzenie figury i osi
     bars = plt.bar(df_sorted["Player Name"], df_sorted["Distance (km)"], color="#FFAF00")
     plt.ylabel("Dystans (km)")
     fig.text(0.05, 0.98, f"vs {session_title}", ha='left', va='bottom', fontsize=18, fontweight='bold')
@@ -30,6 +30,7 @@ def generate_charts(file_path, output_folder="output"):
         height = bar.get_height()
         plt.text(bar.get_x() + bar.get_width()/2, height, f"{height:.2f}", ha='center', va='bottom', fontsize=10)
     plt.text(0.95, 0.95, f"Całkowity dystans drużyny: {total_distance:.2f} km", ha='right', va='top', transform=plt.gca().transAxes, fontsize=12, fontweight='bold', color='black')
+    plt.subplots_adjust(top=0.9)
     plt.xticks(rotation=45)
     plt.grid(axis="y", linewidth=0.5, color='gray', alpha=0.3)
     plt.tight_layout()
