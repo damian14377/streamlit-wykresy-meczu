cedda763e658a2f70eb9c922595663b8fead60f8
damian14377 <damian4ev@gmail.com>
2025-01-31 01:18:55 +0100

    Update generate_charts.py

--- generate_charts.py
+++ generate_charts.py
@@ -24,7 +24,7 @@ def generate_charts(file_path, output_folder="output"):
     plt.figure(figsize=(12, 6))
     bars = plt.bar(df_sorted["Player Name"], df_sorted["Distance (km)"], color="#FFAF00")
     plt.ylabel("Dystans (km)")
-    plt.suptitle(f"vs {session_title}", fontsize=18, fontweight='bold', ha="left")  # Pogrubiony nagłówek
+    fig.text(0.05, 0.98, f"vs {session_title}", ha='left', va='bottom', fontsize=18, fontweight='bold')
     plt.title("Dystans (km)", fontsize=14, pad=10)
     for bar in bars:
         height = bar.get_height()
